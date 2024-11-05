9668151bfc06958bb0c86fb53b462d3d6636039e
ak8986 <ayush4567.raj@gmail.com>
2024-11-06 00:43:10 +0530

    Initial set up complete

---
+++ server/routes/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+    //  const mongoURL = 'mongodb://localhost:27017/voting';
+    // const mongoURL = process.env.MONGODB_URL;
+ const mongoURL = process.env.MONGO_URL_LOCAL
+//setup mongodb connection
+mongoose.connect('mongodb://localhost:27017/voting', { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+});
+// mongoose.connect('mongodb://localhost:27017/voting');
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log('Connected to MongoDB'); 
+});
+
+db.on('error', (err) => {
+    console.log('MongoDB connection error');  
+});
+
+db.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+});  
